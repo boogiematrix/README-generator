9ce0f78b0eae88b4ddab8333806a4dae55a0f53b
Night Chough <chicken.snuggles@gmail.com>
2021-04-06 00:36:06 -0500

    added questions array and rendering template

--- index.js
+++ index.js
@@ -1,25 +1,60 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
-        message: 'installation requirements',
+        message: 'What is the title of your project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'installation instructions',
         name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Provide instructions for use',
+        name: 'usage'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What type of license would you like to use?',
+        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License']
+    },
+    {
+        type: 'input',
+        message: 'What are the guidelines for contribution?',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'What are your instructions for testing?',
+        name: 'test'
+    },
+    {
+        type: 'input',
+        message: 'What is your github user name?',
+        name: 'github'
+    },
+    {
+        type: 'input',
+        message: 'What is your email address',
+        name: 'email'
     }
-    /* installation
-    usage
-    credits
-    license
-    badges
-    features
-    contributing
-    */
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    console.log(fileName);
+    console.log(data)
+    fs.writeFile(fileName, generateMarkdown(data), (err) => {
+        err ? console.log(err) : console.log('success!')
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,7 +13,30 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description
 
+  ## Table of Contents
+  1. [Installation](#Installation)
+  2. [Usage](#Usage)
+  3. [License](#License)
+  4. [Contributing](#Contributing)
+  5. [Tests](#Tests)
+  6. [Questions](#Questions)
+
+  ## Installation
+${data.installation}
+  ## Usage
+${data.usage}
+  ## License
+
+  ## Contributing
+${data.contributing}
+  ## Tests
+${data.test}
+  ## Questions
+  Feel free to contact me with any questions
+[My github](https://github.com/${data.github})
+email: ${data.email}
 `;
 }
 
